107082706a68e6672e9a62c19507d9f5ddc0f959
Gabriel Russ <gabrielruss88@gmail.com>
2022-12-28 13:10:04 -0500

    init

---
+++ src/main/java/com/dontlookatmystuff/dlams/DlamsApplication.java
@@ -0,0 +1,13 @@
+package com.dontlookatmystuff.dlams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DlamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DlamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dontlookatmystuff/dlams/DlamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dontlookatmystuff.dlams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DlamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
